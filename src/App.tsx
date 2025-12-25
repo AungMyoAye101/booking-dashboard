641732f17cc27a44446dfdf979b5c51677f9c29f
Aung Myo Aye <aungmyoaye101@gmail.com>
2025-12-25 20:04:41 +0630

    initial setup.

---
+++ src/App.tsx
@@ -0,0 +1,57 @@
+import { Badge } from "lucide-react"
+import { Button } from "./components/ui/button"
+import { Input } from "./components/ui/input"
+import { Spinner } from "./components/ui/spinner"
+
+import { Moon, Sun } from "lucide-react"
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { useTheme } from "@/components/theme-provider"
+
+export function ModeToggle() {
+  const { setTheme } = useTheme()
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon">
+          <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setTheme("light")}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")}>
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")}>
+          System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
+
+
+const App = () => {
+  return (
+    <div className="p-40 ">
+      <Button>Click me</Button>
+      <Badge>Bad</Badge>
+      <Input></Input>
+      <Spinner></Spinner>
+      <p>Hello</p>
+      <ModeToggle />
+    </div>
+  )
+}
+
+export default App
